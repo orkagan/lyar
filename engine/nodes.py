c907f2e52b13fc571b2efad3a0ea0782621b5ce7
mcpower <mcpower@users.noreply.github.com>
2015-01-16 13:49:45 +1100

    Fixed inconsistent tab styles, added .editorconfig

--- engine/nodes.py
+++ engine/nodes.py
@@ -2,138 +2,138 @@ import html
 
 class Node:
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def render(self, context):
-		raise NotImplementedError()
+    def render(self, context):
+        raise NotImplementedError()
 
 class TextNode(Node):
 
-	def __init__(self, content):
-		self.content = content
+    def __init__(self, content):
+        self.content = content
 
-	def render(self, context):
-		return self.content
+    def render(self, context):
+        return self.content
 
-	def __str__(self):
-		return "TextNode: " + self.content
+    def __str__(self):
+        return "TextNode: " + self.content
 
-	def __repr__(self):
-		return "TextNode"
+    def __repr__(self):
+        return "TextNode"
 
 class PythonNode(Node):
 
-	def __init__(self, code):
-		self.code = code
+    def __init__(self, code):
+        self.code = code
 
-	def render(self, context):
-		try:
-			output = eval(self.code, {}, context)
-		except NameError:
-			raise NameError("Required variable not defined in context.")
-		else:
-			return html.escape(str(output))
+    def render(self, context):
+        try:
+            output = eval(self.code, {}, context)
+        except NameError:
+            raise NameError("Required variable not defined in context.")
+        else:
+            return html.escape(str(output))
 
-	def __str__(self):
-		return "PythonNode: " + self.code
+    def __str__(self):
+        return "PythonNode: " + self.code
 
-	def __repr__(self):
-		return "PythonNode"
+    def __repr__(self):
+        return "PythonNode"
 
 class SafeNode(PythonNode):
-	def render(self, context):
-		try:
-			output = eval(self.code, {}, context)
-		except NameError:
-			raise NameError("Required variable in safe statement not defined in context.")
-		else:
-			return str(output)
+    def render(self, context):
+        try:
+            output = eval(self.code, {}, context)
+        except NameError:
+            raise NameError("Required variable in safe statement not defined in context.")
+        else:
+            return str(output)
 
-	def __str__(self):
-		return "SafeNode: " + self.code
+    def __str__(self):
+        return "SafeNode: " + self.code
 
-	def __repr__(self):
-		return "SafeNode"
+    def __repr__(self):
+        return "SafeNode"
 
 class GroupNode(Node):
 
-	def __init__(self, children=None):
-		if children is not None:
-			self.children = children
-		else:
-			self.children = []
+    def __init__(self, children=None):
+        if children is not None:
+            self.children = children
+        else:
+            self.children = []
 
-	def render(self, context):
-		finalStr = ""
-		for child in self.children:
-			finalStr += child.render(context)
-		return finalStr
+    def render(self, context):
+        finalStr = ""
+        for child in self.children:
+            finalStr += child.render(context)
+        return finalStr
 
-	def addChild(self, node):
-		self.children.append(node)
+    def addChild(self, node):
+        self.children.append(node)
 
-	def __repr__(self):
-		return "GroupNode"
+    def __repr__(self):
+        return "GroupNode"
 
-	def __str__(self):
-		return "GroupNode: " + str(self.children)
+    def __str__(self):
+        return "GroupNode: " + str(self.children)
 
 class IfNode(Node):
 
-	def __init__(self, predicate):
-		self.predicate = predicate
-		self.trueNode = GroupNode()
-		self.falseNode = GroupNode()
+    def __init__(self, predicate):
+        self.predicate = predicate
+        self.trueNode = GroupNode()
+        self.falseNode = GroupNode()
 
-	def addTrueChild(self, node):
-		self.trueNode.addChild(node)
+    def addTrueChild(self, node):
+        self.trueNode.addChild(node)
 
-	def addFalseChild(self, node):
-		self.falseNode.addChild(node)
+    def addFalseChild(self, node):
+        self.falseNode.addChild(node)
 
-	def render(self, context):
-		try:
-			result = eval(self.predicate, {}, context)
-		except NameError:
-			raise NameError("Required variable in if predicate not defined in context. Context is {}".format(context))
-		else:
-			if result:
-				return self.trueNode.render(context)
-			else:
-				return self.falseNode.render(context)
+    def render(self, context):
+        try:
+            result = eval(self.predicate, {}, context)
+        except NameError:
+            raise NameError("Required variable in if predicate not defined in context. Context is {}".format(context))
+        else:
+            if result:
+                return self.trueNode.render(context)
+            else:
+                return self.falseNode.render(context)
 
-	def __repr__(self):
-		left = '...' if self is self.trueNode else str(self.trueNode)
-		right = '...' if self is self.falseNode else str(self.falseNode)
-		return 'IfNode({}, {}, {})'.format(id(self), left, right)
+    def __repr__(self):
+        left = '...' if self is self.trueNode else str(self.trueNode)
+        right = '...' if self is self.falseNode else str(self.falseNode)
+        return 'IfNode({}, {}, {})'.format(id(self), left, right)
 
 
 class ForNode(Node):
-	def __init__(self, iterable, variable):
-		self.loopBlock = GroupNode()
-		self.iterable = iterable
-		self.variable = variable
+    def __init__(self, iterable, variable):
+        self.loopBlock = GroupNode()
+        self.iterable = iterable
+        self.variable = variable
 
-	def addLoopChild(self, node):
-		self.loopBlock.addChild(node)
+    def addLoopChild(self, node):
+        self.loopBlock.addChild(node)
 
-	def render(self, context):
-		try:
-			iterating = eval(self.iterable, {}, context)
-		except NameError:
-			raise NameError("Required variable in for loop iterable not defined in context.")
-		else:
-			out = ""
-			for item in iterating:
-				newContext = dict(context) # new instance
-				newContext[self.variable] = item
-				out += self.loopBlock.render(newContext)
-			return out
+    def render(self, context):
+        try:
+            iterating = eval(self.iterable, {}, context)
+        except NameError:
+            raise NameError("Required variable in for loop iterable not defined in context.")
+        else:
+            out = ""
+            for item in iterating:
+                newContext = dict(context) # new instance
+                newContext[self.variable] = item
+                out += self.loopBlock.render(newContext)
+            return out
 
 class CommentNode(Node):
-	def __init__(self, comment):
-		self.comment = comment
+    def __init__(self, comment):
+        self.comment = comment
 
-	def render(self, context):
-		return ''
+    def render(self, context):
+        return ''
--- engine/template.py
+++ engine/template.py
@@ -5,138 +5,138 @@ from .nodes import TextNode, PythonNode, GroupNode, IfNode, ForNode, SafeNode, C
 DIRECTORY = os.path.join(os.path.dirname(__file__), '..', 'templates')
 
 class ParseException(Exception):
-	pass
+    pass
 
 def split_tokens(s):
 
-	r = re.compile(r'({[{%].*?[%}]})')
-	return r.split(s)
+    r = re.compile(r'({[{%].*?[%}]})')
+    return r.split(s)
 
 def make_dict(label, data):
-	return {'label' : label,
-			'data'  : data
-			}
+    return {'label' : label,
+            'data'  : data
+            }
 
 
 def id_token(token):
-	if token.startswith('{{') and token.endswith('}}'):
-		return make_dict('python', token[2:-2].strip())
-	elif token.startswith('{%') and token.endswith('%}'):
-		inner = token[2:-2].strip().split()
-		if inner[0] == 'include':
-			return make_dict('include', {'path' : inner[1], 'variables' : inner[2:]})
-		elif inner[0] == 'if':
-			predicate = " ".join(inner[1:])
-			return make_dict('if', predicate)
-		elif inner[0] == 'else':
-			return make_dict('else', None)
-		elif ' '.join(inner) == 'end if':
-			return make_dict('end if', None)
-		elif inner[0] == 'for':
-			return make_dict('for', {'variable': inner[1], 'iterable': " ".join(inner[3:])})
-		elif ' '.join(inner) == 'end for':
-			return make_dict('end for', None)
-		elif inner[0] == 'safe':
-			return make_dict('safe', " ".join(inner[1:]))
-		elif inner[0] == 'comment':
-			return make_dict('comment', ' '.join(inner[1:]))
+    if token.startswith('{{') and token.endswith('}}'):
+        return make_dict('python', token[2:-2].strip())
+    elif token.startswith('{%') and token.endswith('%}'):
+        inner = token[2:-2].strip().split()
+        if inner[0] == 'include':
+            return make_dict('include', {'path' : inner[1], 'variables' : inner[2:]})
+        elif inner[0] == 'if':
+            predicate = " ".join(inner[1:])
+            return make_dict('if', predicate)
+        elif inner[0] == 'else':
+            return make_dict('else', None)
+        elif ' '.join(inner) == 'end if':
+            return make_dict('end if', None)
+        elif inner[0] == 'for':
+            return make_dict('for', {'variable': inner[1], 'iterable': " ".join(inner[3:])})
+        elif ' '.join(inner) == 'end for':
+            return make_dict('end for', None)
+        elif inner[0] == 'safe':
+            return make_dict('safe', " ".join(inner[1:]))
+        elif inner[0] == 'comment':
+            return make_dict('comment', ' '.join(inner[1:]))
 
-	else:
-		return make_dict('text', token)
+    else:
+        return make_dict('text', token)
 
 
 def lexer(s):
-	tags = split_tokens(s)
-	tokens = [id_token(tag) for tag in tags]
-	return tokens
+    tags = split_tokens(s)
+    tokens = [id_token(tag) for tag in tags]
+    return tokens
 
 
 def parse(tokens, parent=None, upto=0, parentType=None):
-	if parent is None:
-		parent = GroupNode()
-	while upto < len(tokens):
-		currentToken = tokens[upto]
-		label = currentToken['label'] 
-		if label == 'python':
-			newNode = PythonNode(currentToken['data'])
-			upto += 1
-			parent.addChild(newNode)
-		elif label == 'safe':
-			newNode = SafeNode(currentToken['data'])
-			upto += 1
-			parent.addChild(newNode)
-		elif label == 'text':
-			newNode = TextNode(currentToken['data'])
-			upto += 1
-			parent.addChild(newNode)
-		elif label == 'include':
-			upto += 1
-			includePath = os.path.join(DIRECTORY, currentToken['data']['path'])
-			with open(includePath) as f:
-				parse(lexer(f.read()), parent, 0, parentType) #new file, therefore upto = 0
-		
-		elif label == 'if':
-			newNode = IfNode(currentToken['data'])
-			parent.addChild(newNode)
-			upto += 1
-			ifGroup, newPos = parse(tokens, newNode.trueNode, upto, "if")
-			upto = newPos
-			currentToken = tokens[upto]
-			label = currentToken['label']
-			if label == "else":
-				upto += 1
-				elseGroup, newPos = parse(tokens, newNode.falseNode, upto, "else")
-				upto = newPos
+    if parent is None:
+        parent = GroupNode()
+    while upto < len(tokens):
+        currentToken = tokens[upto]
+        label = currentToken['label'] 
+        if label == 'python':
+            newNode = PythonNode(currentToken['data'])
+            upto += 1
+            parent.addChild(newNode)
+        elif label == 'safe':
+            newNode = SafeNode(currentToken['data'])
+            upto += 1
+            parent.addChild(newNode)
+        elif label == 'text':
+            newNode = TextNode(currentToken['data'])
+            upto += 1
+            parent.addChild(newNode)
+        elif label == 'include':
+            upto += 1
+            includePath = os.path.join(DIRECTORY, currentToken['data']['path'])
+            with open(includePath) as f:
+                parse(lexer(f.read()), parent, 0, parentType) #new file, therefore upto = 0
+        
+        elif label == 'if':
+            newNode = IfNode(currentToken['data'])
+            parent.addChild(newNode)
+            upto += 1
+            ifGroup, newPos = parse(tokens, newNode.trueNode, upto, "if")
+            upto = newPos
+            currentToken = tokens[upto]
+            label = currentToken['label']
+            if label == "else":
+                upto += 1
+                elseGroup, newPos = parse(tokens, newNode.falseNode, upto, "else")
+                upto = newPos
 
-		elif label == "else":
-			if parentType == "if":
-				return parent, upto
-			else:
-				raise ParseException('Found "else" without corresponding "if" statement')
-		elif label == 'end if':
-			if parentType in ("if", "else"):
-				upto += 1
-				return parent, upto
-			else:
-				raise ParseException('Found "end if" without corresponding "if" statement')
-			#consider unmatched if
+        elif label == "else":
+            if parentType == "if":
+                return parent, upto
+            else:
+                raise ParseException('Found "else" without corresponding "if" statement')
+        elif label == 'end if':
+            if parentType in ("if", "else"):
+                upto += 1
+                return parent, upto
+            else:
+                raise ParseException('Found "end if" without corresponding "if" statement')
+            #consider unmatched if
 
-		elif label == 'for':
-			newNode = ForNode(currentToken['data']['iterable'], currentToken['data']['variable'])
-			parent.addChild(newNode)
-			upto += 1
-			forGroup, newPos = parse(tokens, newNode.loopBlock, upto, "for")
-			upto = newPos
-			currentToken = tokens[upto]
-			label = currentToken['label']
-		elif label == 'end for':
-			if parentType == 'for':
-				upto += 1
-				return parent, upto
-			else:
-				raise ParseException('Found "end for" without corresponding "for" statement')
+        elif label == 'for':
+            newNode = ForNode(currentToken['data']['iterable'], currentToken['data']['variable'])
+            parent.addChild(newNode)
+            upto += 1
+            forGroup, newPos = parse(tokens, newNode.loopBlock, upto, "for")
+            upto = newPos
+            currentToken = tokens[upto]
+            label = currentToken['label']
+        elif label == 'end for':
+            if parentType == 'for':
+                upto += 1
+                return parent, upto
+            else:
+                raise ParseException('Found "end for" without corresponding "for" statement')
 
-		elif label == 'comment':
-			upto += 1
-			newNode = CommentNode(currentToken['data'])
-			parent.addChild(newNode)
+        elif label == 'comment':
+            upto += 1
+            newNode = CommentNode(currentToken['data'])
+            parent.addChild(newNode)
 
 
 
-	return parent, upto
+    return parent, upto
 
 def render_page(fileName, context):
-	filePath = os.path.join(DIRECTORY, fileName)
-	with open(filePath) as f:
-		return parse(lexer(f.read()))[0].render(context)
+    filePath = os.path.join(DIRECTORY, fileName)
+    with open(filePath) as f:
+        return parse(lexer(f.read()))[0].render(context)
 
 # if __name__ == '__main__':
-# 	test = """
-# 			<html>
-# 			<head><title>TitleText</title><head>
-# 			{{ 4+2 }} 
-# 			{% if False %}
-# 			Hello
-# 			{% end if %}
-# 			more text"""
-# 	print(parse(lexer(test))[0].render({}))
+#   test = """
+#           <html>
+#           <head><title>TitleText</title><head>
+#           {{ 4+2 }} 
+#           {% if False %}
+#           Hello
+#           {% end if %}
+#           more text"""
+#   print(parse(lexer(test))[0].render({}))
